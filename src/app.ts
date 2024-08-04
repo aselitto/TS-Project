9901521d8d82668927897753e891490677fb02b0
aselitto <aselitto@gmail.com>
2024-08-04 13:05:15 -0400

    Initial Commit

---
+++ src/app.ts
@@ -0,0 +1,7 @@
+import { ProjectInput } from './components/ProjectInput';
+import { ProjectList } from './components/ProjectList';
+
+// Instantiate main components
+new ProjectInput();
+new ProjectList('active');
+new ProjectList('finished');
